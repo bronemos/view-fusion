move reshaping to model, fix output scaling

data/dataset.py
@@ -36,7 +36,7 @@ def create_webdataset(path, mode, start_shard=0, end_shard=12):
         images = [sample[f"{i:04d}.png"] for i in range(24)]
         images = np.stack(images, 0)  # .astype(np.float32)
 
-        images = 2 * rearrange(images, "v h w c -> (v c) h w") - 1
+        images = rearrange(images, "v h w c -> v c h w")  # 2 * ... -1
 
         result = {
             "images": images,

main.py
@@ -184,11 +184,13 @@ if __name__ == "__main__":
     masked_cnt = 3
     view_cnt = 24
     in_chann = 3
-    test_eval = False
+    test_eval = True
 
     while True:
         for batch in train_loader:
             it += 1
+            log_dict = dict()
+
             if rank == 0:
                 pass
 
@@ -197,34 +199,23 @@ if __name__ == "__main__":
                 it > 0 and validate_every > 0 and (it % validate_every) == 0
             ):
                 print("Running evaluation...")
-                eval_dict = dict()
                 images = val_vis_data["images"].to(device)
-                print(torch.max(images), torch.min(images))
-                masked_ids = torch.randint(24, size=(masked_cnt,))
-                mask = rearrange(
-                    torch.zeros_like(images),
-                    "b (v c) h w -> b v c h w",
-                    v=view_cnt,
-                    c=in_chann,
-                )
-                mask[:, masked_ids, :, :, :] = 1
-                mask = rearrange(mask, "b v c h w -> b (v c) h w")
-                y_t, ret_arr = model.generate(images, mask)
 
-                generated_images = rearrange(
-                    ret_arr, "b (v c) h w -> (v b) c h w", v=view_cnt, c=in_chann
-                )
-
-                for i, generated_image in enumerate(generated_images):
-                    eval_dict[f"generated_images_{i}"] = wandb.Image(
-                        make_grid(
-                            generated_images,
-                            nrow=generated_images.shape[0] // view_cnt,
-                        )
+                y_t, generated_batch = model.generate(images, masked_cnt)
+                log_dict["input_batch"] = wandb.Image(
+                    make_grid(
+                        rearrange(images, "b v c h w -> (v b) c h w"),
+                        nrow=images.shape[0],
                     )
+                )
 
-                if args.wandb:
-                    wandb.log(eval_dict, step=it)
+                for i, generated_images in enumerate(generated_batch):
+                    grid_images = rearrange(
+                        generated_images, "s v c h w -> (v s) c h w"
+                    )
+                    log_dict[f"generated_images_{i}"] = wandb.Image(
+                        make_grid(grid_images, nrow=generated_images.shape[0])
+                    )
 
             new_lr = lr_scheduler.get_cur_lr(it)
             for param_group in optimizer.param_groups:
@@ -241,13 +232,12 @@ if __name__ == "__main__":
             time_elapsed += time.perf_counter() - t0
 
             if log_every > 0 and it % log_every == 0:
-                log_dict = dict()
                 log_dict["t"] = time_elapsed
                 log_dict["lr"] = new_lr
                 log_dict["loss"] = loss.item()
 
-                if args.wandb:
-                    wandb.log(log_dict, step=it)
+            if args.wandb and log_dict:
+                wandb.log(log_dict, step=it)
 
             if it > max_it:
                 if rank == 0:

models/palette.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 
 from inspect import isfunction
+from einops import rearrange
 from functools import partial
 from tqdm import tqdm
 from torch import nn
@@ -101,7 +102,13 @@ class PaletteViewSynthesis(nn.Module):
         return model_mean + noise * (0.5 * model_log_variance).exp()
 
     @torch.no_grad()
-    def generate(self, y_0, mask=None, sample_num=8):
+    def generate(self, images, masked_cnt, sample_num=8):
+        masked_ids = torch.randint(24, size=(masked_cnt,))
+        mask = torch.zeros_like(images)
+        mask[:, masked_ids, :, :, :] = 1
+        mask = rearrange(mask, "b v c h w -> b (v c) h w")
+        y_0 = rearrange(images, "b v c h w -> b (v c) h w")
+
         b, *_ = y_0.shape
 
         assert (
@@ -110,6 +117,7 @@ class PaletteViewSynthesis(nn.Module):
         sample_inter = self.num_timesteps // sample_num
 
         y_t = torch.randn_like(y_0)
+        y_t = (y_t - torch.min(y_t)) / (torch.max(y_t) - torch.min(y_t))
         ret_arr = y_t
         for i in tqdm(
             reversed(range(0, self.num_timesteps)),
@@ -118,14 +126,26 @@ class PaletteViewSynthesis(nn.Module):
         ):
             t = torch.full((b,), i, device=y_0.device, dtype=torch.long)
             y_t = self.p_sample(y_t, t)
+            y_t = (y_t - torch.min(y_t)) / (torch.max(y_t) - torch.min(y_t))
             if mask is not None:
                 y_t = y_0 * (1.0 - mask) + mask * y_t
             if i % sample_inter == 0:
                 ret_arr = torch.cat([ret_arr, y_t], dim=0)
+
+        ret_arr = rearrange(
+            ret_arr,
+            "(s b) (v c) h w -> b s v c h w",
+            b=images.shape[0],
+            s=sample_num + 1,
+            v=images.shape[1],
+            c=images.shape[2],
+        )
         return y_t, ret_arr
 
     def forward(self, y_0, mask=None, noise=None):
         # sampling from p(gammas)
+        y_0 = rearrange(y_0, "b v c h w -> b (v c) h w")
+
         b, *_ = y_0.shape
         t = torch.randint(1, self.num_timesteps, (b,), device=y_0.device).long()
         gamma_t1 = extract(self.gammas, t - 1, x_shape=(1, 1))